be39a0a9abadbb960c1a786b06ee5e0b45d77583
Grant Bridges <128187273+gbridges-capsher@users.noreply.github.com>
2024-12-01 20:53:32 -0600

    Day 2 solution

--- day_1_solutions.py
+++ day_1_solutions.py
@@ -34,15 +34,33 @@ class Day1Solutions(BaseAoCDaySolutions):
 
     def part_2(self):
         with open(self.input_file, 'r') as file:
-            # track two lists for each column
             left_list = []
-            right_list = []
+            right_dict = dict()
 
-            # for each line, store the left and right value in their
-            # corresponding lists (assume separation by '   ')
+            # read data in...
             for line in file.readlines():
                 vals = line.split('   ')
                 left_val = int(vals[0])
                 right_val = int(vals[1])
+
+                # store left_val in left_list dumbly
+                left_list.append(left_val)
+
+                # treat right_vals as a dictionary, tracking the number of times
+                # each shows up
+                if (right_val not in right_dict):
+                    right_dict[right_val] = 0
+                right_dict[right_val] += 1
+            
+            # Check over left list and count the number of times that
+            # same number shows up in right list, then total up "similarity score"
+            # as left_val * right_dict[val] (i.e. its occurrences in right list)
+            total_similarity_score = 0
+            for val in left_list:
+                if (val in right_dict):
+                    similarity_score = val * right_dict[val]
+                    total_similarity_score += similarity_score
+
+            return total_similarity_score
     
-Day1Solutions().run_solution_part(1)
+Day1Solutions().run_solutions()
